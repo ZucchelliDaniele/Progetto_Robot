fde4b3dcb62b4f6bc178ce0c1c75d30ff0d4a754
Zucchelli Daniele <zucchelli.daniele.studente@itispaleocapa.it>
2023-10-26 22:34:51 +0200

    Add files via upload
    WebSite to control the mecanum wheel robot, TailWind css

---
+++ WebSite/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'black': '#000000',
+      'sky' : '#0c4a6e',
+      'white-sky': '#ecfeff',
+      'gray': '#121212',
+      'black-gray' : '#0d0d0d'
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+}
